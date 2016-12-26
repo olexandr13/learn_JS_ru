85c8585205b744fe29af58c91188f32d554c690e
olexandr13 <olexandr13@gmail.com>
2016-12-26 23:02:58 +0200

    10th commit

---
+++ 4-data-structures/5-array.js
@@ -0,0 +1,55 @@
+var goods = [1,2,3,4,5]
+console.log(goods[goods.length-1])
+
+
+goods.push('computer');
+
+
+var styles = ['Jazz', 'Blues'];
+console.log(styles);
+styles.push('rock-n-roll');
+console.log(styles);
+styles[styles.length-2] = 'clasic';
+console.log(styles);
+alert(styles.shift());
+console.log(styles);
+styles.unshift('rap', 'raggy');
+console.log(styles);
+
+
+var arr = ["Яблоко", "Апельсин", "Груша", "Лимон"];
+min = 0;
+max = arr.length - 1;
+function randomizer() {
+    var rand = min + Math.floor(Math.random() * (max + 1 - min));
+    arr[rand];
+}
+
+
+
+function arraySum() {
+    var array = [];
+    var number;
+    var convertedNumber;
+    do {
+        number = prompt('Enter number', '')
+        array.push(+number);
+    } while (!(number == null || isNaN(+number) || number === ''));
+    array.pop();
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+        sum += array[i]
+    }
+    return sum;
+}
+
+
+arr = ["test", 2, 1.5, false];
+function find(arr, value) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === value) return i
+    }
+    return -1;
+}
+
+
