e3cad4eee37aa01a74d74b544aa31ade83e973d5
Oleksandr Pelykh <Oleksandr.Pelykh@playtech.com>
2016-12-28 15:45:08 +0200

    array-method 2nd task

--- 4-data-structures/6-array-methods.js
+++ 4-data-structures/6-array-methods.js
@@ -1,9 +1,8 @@
 var obj = {
     className: 'open menu'
 };
-var arr;
 function addClass(obj, cls) {
-    arr = obj.className.split(' ');
+    var arr = obj.className.split(' ');
     for (var i = 0; i < arr.length; i++) {
         if (arr[i] == cls) {
             return;
@@ -12,4 +11,33 @@ function addClass(obj, cls) {
     arr.push(cls);
     arr = arr.join(' ');
     obj.className = arr;
+    return obj;
 }
+
+
+function camelize(str) {
+    var arr = str.split('-');
+    var newArrayElement;
+    var newArray = [];
+    newArray.push(arr[0]);
+    for (var i = 1; i < arr.length; i++) {
+        newArrayElement = arr[i][0].toUpperCase() + arr[i].slice(1);
+        newArray.push(newArrayElement);
+    }
+    return newArray.join('');
+};
+
+
+function removeClass(obj, cls) {
+    var arr = obj.className.split(' ');
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == cls) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    arr = arr.join(' ');
+    obj.className = arr;
+};
+
+
