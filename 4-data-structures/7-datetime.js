25a3e036acb03e3c7f3e09275d83822780b70b3d
Oleksandr Pelykh <Oleksandr.Pelykh@playtech.com>
2016-12-30 17:42:56 +0200

    few tasks. date

--- 4-data-structures/7-datetime.js
+++ 4-data-structures/7-datetime.js
@@ -1,3 +1,4 @@
+// Имя дня недели
 function getWeekDay(date) {
     var day = date.getDay();
     if (day == 0) return 'нд';
@@ -7,6 +8,28 @@ function getWeekDay(date) {
     if (day == 4) return 'чт';
     if (day == 5) return 'пт';
     if (day == 6) return 'сб';
+}
+
+
+// День указанное количество дней назад
+function getDateAgo(date, days) {
+    var date2 = new Date(date);
+    date2.setDate(date2.getDate() - days);
+    return date2.getDate();
+    // return date2;
+}
+
+
+// Количество дней в месяце
+function getLastDayOfMonth(year, month) {
+    var date = new Date(year, month);
+    for (var i = 1; i <= 31; i++) {
+        var newDate = new Date(date);
+        newDate.setDate(newDate.getDate() + i);
+        if (newDate.getDate() == 1) {
+            return i;
+        }
+    }
 }
 
 
