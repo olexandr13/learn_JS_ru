b33adf8bbdf17d206dd1dbef34aeb5a4c3692ed0
Oleksandr Pelykh <Oleksandr.Pelykh@playtech.com>
2016-12-30 18:32:58 +0200

    date 9th task

--- 4-data-structures/7-datetime.js
+++ 4-data-structures/7-datetime.js
@@ -33,3 +33,48 @@ function getLastDayOfMonth(year, month) {
 }
 
 
+//Сколько секунд уже прошло сегодня?
+function getSecondsToday() {
+    var date = new Date();
+    return (date.getHours()*3600 + date.getMinutes()*60 + date.getSeconds());
+}
+
+
+//Сколько секунд - до завтра?
+function getSecondsToTomorrow() {
+    var date = new Date();
+    var tomorrowDate = new Date(date.getFullYear(), date.getMonth(), date.getDate()+1);
+    return ((tomorrowDate - date)/1000).toFixed(0);
+}
+
+
+//Вывести дату в формате дд.мм.гг
+function formatDate(date) {
+    var day = date.getDate();
+    var month = date.getMonth()+1;
+    var year = date.getFullYear();
+    day = (day < 10) ? '0' + day : day;
+    month = (month < 10) ? '0' + month : month;
+    return (day + '.' + month + '.' + year.toString().slice(-2));
+}
+
+
+//Относительное форматирование даты
+function formatDate(date) {
+    var currentDate = new Date();
+    if (currentDate - date < 1000) return 'только что';
+    else if (currentDate - date < 60*1000) return (currentDate - date)/1000 + ' сек. назад';
+    else if (currentDate - date < 60*60*1000) return (currentDate - date)/(1000*60) + ' мин. назад';
+    else {
+        var day = date.getDate();
+        var month = date.getMonth()+1;
+        var year = date.getFullYear();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        minutes = (minutes < 10) ? '0' + minutes : minutes;
+        hours = (hours < 10) ? '0' + hours : hours;
+        day = (day < 10) ? '0' + day : day;
+        month = (month < 10) ? '0' + month : month;
+        return (day + '.' + month + '.' + year.toString().slice(-2) + ' ' + hours + ':' + minutes);
+    }
+}
